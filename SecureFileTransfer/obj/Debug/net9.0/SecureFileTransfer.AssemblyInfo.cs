a119298d6bcdf731ed3e1dc8cba4f925a96e8daf
Arihant Singh <Shubh1229@users.noreply.github.com>
2026-04-23 15:57:38 -0400

    debugger added

--- SecureFileTransfer/obj/Debug/net9.0/SecureFileTransfer.AssemblyInfo.cs
+++ SecureFileTransfer/obj/Debug/net9.0/SecureFileTransfer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecureFileTransfer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057129ddd41a0bc578119903c440c2d4534b5c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68c35705ff74d8dda435806458483da4100e0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureFileTransfer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureFileTransfer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
